b1e4d9164a9bc5f81c9afd4bb40be82c1ae36ba9
David Benton <djb@dajabe.nz>
2023-03-19 15:18:31 +1300

    Create base page

--- mbpr-mem-list.php
+++ mbpr-mem-list.php
@@ -9,4 +9,22 @@
  **/
 
 
+
+// Insert a menu into Admin > Settings
+add_action( 'admin_menu', 'mem_list_menu' );
+
+function mem_list_menu() {
+	add_options_page( 'Memberlist to Mailer', 'Import Memberlists', 'manage_options', 'mbpr-mem-list', 'mem_list_main' );
+}
+
+// Page of doing
+function mem_list_main() {
+	if ( ! current_user_can( 'manage_options' ) ) {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<h1>Memberlists to mailing lists</h1>';
+	echo '<div class="wrap">';
+	echo '<p>Select from the lists below to add specific user subscriptions to </p>';
+	echo '</div>';
+}
 ?>
