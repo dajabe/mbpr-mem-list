5c5eec5d4d78d967d95d0fe2644dff36b032b9b3
David Benton <djb@dajabe.nz>
2023-03-19 14:16:10 +1300

    Initial commit

---
+++ mbpr-mem-list.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: mbpr-mem-list
+ * Plugin URI: https://github.com/dajabe/mbpr-mem-list
+ * Description: Plugin for taking memberpress user lists and adding them to specific mailing lists.
+ * Version: 0.1
+ * Author: djb
+ * Author URI: https://dajabe.nz/
+ **/
+
+
+?>
